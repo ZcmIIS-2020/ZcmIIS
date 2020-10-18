ecf0a5be8ed9b81d92a487c2d8267dce8f0fd9d0
dff <1257537621@qq.com>
2020-10-18 15:49:11 +0800

    添加右鍵菜單，刪改部分代碼

--- www/js/sensors.js
+++ www/js/sensors.js
@@ -25,10 +25,7 @@ function documentMenu(parent, ev) {
         }
 
         ul.appendChild(li);
-
-
     }
-
 
     //优化菜单在靠近窗口右端的显示
     if (ev.clientX + 120 >= parent.documentElement.clientWidth) {
@@ -107,8 +104,109 @@ function addNewSensor() {
     });
 }
 
-function sensorMenu() {
+function sensorMenu(parent, ev) {
     var menuData = [
-        { id: '' }
+        { id: 'modifySensor', innerHTML: '编辑' },
+        { id: 'deleteSensor', innerHTML: '删除' },
     ]
+    ev.preventDefault();
+    //ev.stopPropagation();
+    //ev.stopImmediatePropagation();
+    var x = ev.clientX;
+    var y = ev.clientY;
+
+    var div = document.createElement('div');
+    div.className = 'menu';
+
+    var ul = document.createElement('ul');
+    div.appendChild(ul);
+
+    for (var i = 0; i < menuData.length; i++) {
+        var li = document.createElement('li');
+        li.id = menuData[i].id;
+        li.innerHTML = menuData[i].innerHTML;
+        switch (li.id) {
+            case 'modifySensor':
+                li.onclick = modifySensor; //函数后面不能加()，因为会先执行一遍函数
+                break;
+
+            case 'deleteSensor':
+                li.onclick = function() {
+                    //console.log(ev.target.parentNode.id);
+                    var selectID = ev.target.parentNode.id;
+                    var URL = "/ZcmDB|sensor_info|delete from sensors where name = '" + selectID + "';|";
+                    window.deleteDB(URL);
+                    $("#" + selectID + "").remove();
+                };
+                break;
+        }
+
+        ul.appendChild(li);
+    }
+
+    //优化菜单在靠近窗口右端的显示
+    if (ev.clientX + 120 >= parent.documentElement.clientWidth) {
+        x = parent.documentElement.clientWidth - 120
+    }
+    //优化菜单在靠近窗口下端的显示
+    if (ev.clientY + 40 >= parent.documentElement.clientHeight) {
+        y = parent.documentElement.clientHeight - 40
+    }
+    div.style.left = x;
+    div.style.top = y;
+    parent.body.appendChild(div);
+}
+
+function modifySensor() {
+    layui.use('layer', function() {
+        var layer = layui.layer;
+        layer.open({
+            title: "编辑传感器",
+            type: 2,
+            closeBtn: 1,
+            area: ['500px', '450px'], //['500px', '350px']
+            shade: 0.2,
+            shadeClose: true,
+            resize: false,
+            btn: ['确定', '取消'],
+            scrollbar: false,
+            scrolling: 'no',
+            id: 'LAY_1', //设定一个id，防止重复弹出
+            content: ['edit_sensor.html?selectID=' + selectID, 'no'],
+            yes: function(index, layero) {
+                let loading = layer.load(1);
+                body = layui.layer.getChildFrame('body', index);
+                let name = body.find("#name").val().trim();
+                //console.log(name)
+                let ip = body.find("#ip").val().trim();
+                //console.log(ip)
+                let channel = body.find("#channel").val().trim();
+                //console.log(channel)
+                let number = body.find("#number").val().trim();
+                //console.log(number)
+                let type = body.find("#type").val().trim();
+                //console.log(type)
+                let high_alert = body.find("#high_alert").val().trim();
+                let low_alert = body.find("#low_alert").val().trim();
+                let check_parameter = body.find("#check_parameter").val().trim();
+                let replace_sensor_id = body.find("#replace_sensor_id").val().trim();
+                layer.close(loading);
+                layer.close(index);
+
+                let url1 = '/ZcmDB|sensor_info|update sensors set name = "' + name + '", ip = "' + ip + '", channel = "' + channel + '", number = "' + number + '", high_alert = "' + high_alert + '", low_alert = "' + low_alert + '", check_parameter = "' + check_parameter + '", replace_sensor_id = "' + replace_sensor_id + '" where name = "' + name + '";|';
+                window.updateDB(url1);
+
+            },
+            btn2: function(index, layero) {
+                layer.close(index);
+            },
+            cancel: function(index, layero) {
+                layer.close(index)
+                return false;
+            },
+            end: function() {
+                location.reload();
+            }
+        });
+    });
 }
