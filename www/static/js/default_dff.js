8e42bf6fa6df1a4a003073cd8ddb7fa74d99068a
dff <1257537621@qq.com>
2020-11-21 20:34:13 +0800

    小修metro.html

--- www/static/js/default_dff.js
+++ www/static/js/default_dff.js
@@ -215,7 +215,7 @@ $(document).ready(function() {
         var ws = new WebSocket(url); //申请一个WebSocket对象，参数是服务端地址，同http协议使用http://开头一样，WebSocket协议的url使用ws://开头，另外安全的WebSocket协议使用wss://开头
         ws.onopen = function() {　　 //当WebSocket创建成功时，触发onopen事件
             console.log("socket connected");　　
-            $("#webSocket").css("background-image", "url(../images/socket_on.png)");
+            $("#webSocket").css("background-image", "url(../static/images/socket_on.png)");
         }
         ws.onmessage = function(e) {　　 //当客户端收到服务端发来的消息时，触发onmessage事件，参数e.data包含server传递过来的数据　　
             console.log(e.data);　
@@ -244,12 +244,12 @@ $(document).ready(function() {
         }
         ws.onclose = function(e) {　　 //当客户端收到服务端发送的关闭连接请求时，触发onclose事件　　
             console.log("socket close");
-            $("#webSocket").css("background-image", "url(../images/socket_off.png)");
+            $("#webSocket").css("background-image", "url(../static/images/socket_off.png)");
             reconnect();
         }
         ws.onerror = function(e) {　　 //如果出现连接、处理、接收、发送数据失败的时候触发onerror事件　
             console.log("socket error");
-            $("#webSocket").css("background-image", "url(../images/socket_error.png)");
+            $("#webSocket").css("background-image", "url(../static/images/socket_error.png)");
             reconnect();
         }
     }
